c96bc57d90e06bc9eedbc7e3aef28cbd2127fa1f
Mani-sh24 <manish.adhikari.2402@gmail.com>
2026-05-10 22:36:58 +0530

    Stupid Comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include<string>
 int main()
 {
-    // Cache<int , int> mc;
-    // mc.put(1, 2 , 100); // key 1 value 2 expiry 100seconds auto = no exipiration
-    // mc.put(2, 4); // no expiration
-    // mc.get(1);
-    // mc.get(2);
-    // mc.cleanExpired(); // manually clean expired keys from heap
-    // Cache<std::string , int> mc1(true , 10); // auto cleaning expired keys every 10 seconds -> flag to turn on auto clean , interval
+    Cache<int , int> mc;
+    mc.put(1, 2 , 100); // key 1 value 2 expiry 100seconds auto = no exipiration
+    mc.put(2, 4); // no expiration
+    mc.get(1);
+    mc.get(2);
+    mc.cleanExpired(); // manually clean expired keys from heap
+    Cache<std::string , int> mc1(true , 10); // auto cleaning expired keys every 10 seconds -> flag to turn on auto clean , interval
     
     return 0;
 }
